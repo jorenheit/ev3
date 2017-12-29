50e9db65da6e94533e44846792b03f53d3f59125
Joren Heit <jorenheit@gmail.com>
2017-12-29 16:31:46 +0100

    updated main

--- project/main.cc
+++ project/main.cc
@@ -5,23 +5,22 @@
 
 using namespace ev3dev;
 
-void drive(large_motor &m, int speed, int pos)
+void drive(large_motor &m1, large_motor &m2, int speed, int pos)
 {
-    m.set_speed_sp(speed);
-    m.set_position_sp(pos).run_to_rel_pos();
+    m1.set_speed_sp(speed);
+    m1.set_position_sp(pos).run_to_rel_pos();
 
-    while (m.state().count("running"))
+    m2.set_speed_sp(speed);
+    m2.set_position_sp(pos).run_to_rel_pos();
+
+    while (m1.state().count("running") || m2.state().count("running"))
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 
 int main(int argc, char **argv)
 {
-    auto m = large_motor{OUTPUT_A};
-    std::cout << m.ramp_up_sp() << '\n';
-    std::cout << m.ramp_down_sp() << '\n';
-    m.set_ramp_up_sp(1000);
-    m.set_ramp_down_sp(1000);
-    drive(m, std::stoi(argv[1]), std::stoi(argv[2]));
-    std::cout << m.position() << '\n';
+    auto m1 = large_motor{OUTPUT_B};
+    auto m2 = large_motor{OUTPUT_C};
+    drive(m1, m2, std::stoi(argv[1]), std::stoi(argv[2]));
 }
